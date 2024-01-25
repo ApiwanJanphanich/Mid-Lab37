933248f5a98981429c329a557edccef8253b7329
ApiwanJanphanich <gamebp02@hotmail.com>
2024-01-25 10:34:12 +0700

    commit3

--- book_app/models.py
+++ book_app/models.py
@@ -6,22 +6,10 @@ class d_type(models.Model):
     def __str__(self):
         return f"{self.type_name}"
 
-class drug(models.Model):
-    drug_id = models.AutoField(primary_key=True)
-    drug_name = models.CharField(max_length=255)
-    drug_type = models.ForeignKey(d_type, on_delete=models.CASCADE)
-    drug_qty = models.IntegerField()
-    drug_exp = models.DateField()
-    def __str__(self):
-        return f"{self.drug_name} , {self.drug_exp}"
-    
-class uesrbuy(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    tal = models.CharField(max_length=255)
-    def __str__(self):
-        return f"{self.username} , {self.tal}"
-    
 
+class book_type(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    photo = models.ImageField()
     
